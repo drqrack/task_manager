08e2c85683f38b5ad3dfcadfc2c3f1fd193ad6b6
chris <drqrack@gmail.com>
2025-08-28 12:35:58 +0000

    Solid changes

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ app.title("Task Manager")
 app.geometry("")
 app.resizable(True, True)
 
+app.rowconfigure(0, weight=1)
+app.columnconfigure(0, weight=1)
+
 ui.show_all_tasks_frame(app)
 
 app.mainloop()
--- ui.py
+++ ui.py
@@ -52,9 +52,9 @@ def show_add_task_frame(app):
 
     entry = tk.Entry(master=frame)
     entry.grid()
-    
+
     btn = tk.Button(master=frame, text="Submit", command=lambda:submit_task(entry.get(), app))
-    btn.grid()
+    btn.grid(row=2, column=0, columnspan=2, pady=10)
     btn_cancel = tk.Button(master=frame, text="Cancel", command=lambda: frame.destroy())
     btn_cancel.grid(row=2, column=1)
 
@@ -64,19 +64,22 @@ def show_all_tasks_frame(app):
     frame = tk.Frame(master=app)
     frame.grid(row=0, column=0, sticky="nsew", padx=20, pady=20)
 
+    label = tk.Label(master=frame, text="Your Tasks", font=("Arial", 14, "bold"))
+    label.grid(row=0, column=0, columnspan=3, pady=10)
+
     tasks = commands.get_tasks().to_list()
     for task in tasks:
         checkbtn = tk.Checkbutton(master=frame, text=task["title"])
-        checkbtn.grid(row=tasks.index(task), column=0)
+        checkbtn.grid(row=tasks.index(task)+1, column=1, sticky="w", padx=5, pady=5)
 
         edit_btn = tk.Button(master=frame, text="Edit", command=partial(show_edit_task_frame, task, app))
-        edit_btn.grid(row=tasks.index(task), column=1)
+        edit_btn.grid(row=tasks.index(task)+1, column=2, padx=5, pady=5)
 
         delete_btn = tk.Button(master=frame, text="Delete", command=partial(handle_delete, task["_id"], app))
-        delete_btn.grid(row=tasks.index(task), column=2)
+        delete_btn.grid(row=tasks.index(task)+1, column=3, padx=5, pady=5)
 
-    add_btn = tk.Button(master=frame, text="Add Task", command=lambda: show_add_task_frame(app))
-    add_btn.grid(row=len(tasks) + 1, column=0, padx=10, pady=10)
+    add_btn = tk.Button(master=frame, width=12, text="Add Task", command=lambda: show_add_task_frame(app))
+    add_btn.grid(row=len(tasks) + 1, column=0, columnspan=3, pady=15)
 
     frame.tkraise()
 
